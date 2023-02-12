4c6943b84f2dfbdcc13a432ce85c6590cd1bcf4c
Alex Morgan <alexbmorgan45@icloud.com>
2023-02-12 03:00:04 +0000

    Security Patch

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ from .secrets import *
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
